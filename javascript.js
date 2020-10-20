578dac343b8d66f38f36063df9913ec91bab22cb
thegeek101 <63245705+thegeek101@users.noreply.github.com>
2020-10-20 11:57:31 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ class PageHeader extends HTMLElement {
 
 class PageFooter extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = '<a href="https://account.xbox.com/en-gb/profile?gamertag=MCHubSite" class="green" style="float:left; font-size: 28px;">Xbox</a><a href="https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg" class="red" style="float:right; font-size: 28px;">YouTube</a><br><br><div style="font-size: 24; text-align: center; display: inline;"><p class="white" style="display: inline;"> Suggest a </p><a style="display: inline;" href="https://forms.gle/qCJSTPwh7tZ5esb98">tip </a><p class="white" style="display: inline;">or a </p><a style="display: inline;" href="https://forms.gle/eAo3zhGsSRm81ja88">seed</a><p class="white" style="display: inline;">!</p></div><br><p class="white">This website uses cookies from Google to enable marketing services such as Google Analytics. Your data is completely anonymous.';
+    this.innerHTML = '<p class="white">This website uses cookies from Google to enable marketing services such as Google Analytics. Your data is completely anonymous.';
   }
 }
 
